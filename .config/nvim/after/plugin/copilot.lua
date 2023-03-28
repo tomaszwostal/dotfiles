5f2e49e933d9fc8113f4101451f26d644c7495f9
Tomasz Wostal <tomasz.wostal@gmail.com>
2023-03-28 17:24:44 +0200

    Fix node version problems

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -43,3 +43,5 @@
 -- })
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.g.copilot_node_command = "~/.nvm/alias/default"
+
